2dbf92fb057c4000591ebd1649e985955a5ef1ab
grekuu <91375853+grekuu@users.noreply.github.com>
2023-05-17 12:06:44 +0200

    project finished

--- backend/data.js
+++ backend/data.js
@@ -1,31 +1,47 @@
-var express = require('express');
-var mysql = require('mysql');
-var cors = require('cors');
+var express = require('express')
+var mysql = require('mysql')
+var cors = require('cors')
+var bodyParser = require('body-parser')
 
-var app = express();
-var port = 4200;
+var app = express()
+var port = 4200
 
 app.use(cors())
+app.use(bodyParser.json())
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "zadanie_08"
-});
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zadanie_08',
+})
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected to MySQL database");
-});
+con.connect(function (err) {
+    if (err) throw err
+    console.log('Connected to MySQL database')
+})
 
-app.get('/', function(req, res) {
-  con.query("SELECT * FROM `table`", function (err, result) {
-    if (err) throw err;
-    res.json(result);
-  });
-});
+app.get('/', function (req, res) {
+    con.query('SELECT * FROM `table`', function (err, result) {
+        if (err) throw err
+        res.json(result)
+    })
+})
 
-app.listen(port, function() {
-  console.log("Server is running on port " + port);
-});
+app.post('/addUser', function (req, res) {
+    var user = req.body
+
+    con.query(
+        'INSERT INTO `table` (autor, data_dodania, tresc) VALUES (?, ?, ?)',
+        [user.autor, user.data_dodania, user.tresc],
+        function (err, result) {
+            if (err) throw err
+            console.log('User added successfully')
+            res.sendStatus(200)
+        }
+    )
+})
+
+app.listen(port, function () {
+    console.log('Server is running on port ' + port)
+})
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,73 @@
-import "./index.css"
-import axios from "axios"
-import {useEffect} from "react"
+import './index.css'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function App() {
+    const [users, setUsers] = useState([])
+    const [autor, setAutor] = useState('')
+    const [dataDodania, setDataDodania] = useState('')
+    const [tresc, setTresc] = useState('')
 
-  function fetchUsers(){
-    axios.get("http://localhost:4200/").then((res) => {
-      console.log(res.data)
-    })
-  }
+    function fetchUsers() {
+        axios.get('http://localhost:4200/').then((res) => {
+            console.log(res.data)
+            setUsers(res.data)
+        })
+    }
 
-  useEffect(() => {
-    fetchUsers()
-  }, [])
+    function addUser() {
+        const newUser = {
+            autor: autor,
+            data_dodania: dataDodania,
+            tresc: tresc,
+        }
 
-  return (
-    <div className="container">
-      <input type="text" placeholder="autor" /> <br />
-      <input type="text" placeholder="data dodania" /> <br />
-      <input type="text" placeholder="tresc" /> <br />
-      <button>Dodaj</button>
-    </div>
-  );
+        axios.post('http://localhost:4200/addUser', newUser).then((res) => {
+            console.log(res.data)
+            fetchUsers()
+        })
+
+        setAutor('')
+        setDataDodania('')
+        setTresc('')
+    }
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
+    return (
+        <div className="container">
+            <input type="text" placeholder="autor" onChange={(e) => setAutor(e.target.value)} value={autor} /> <br />
+            <input
+                type="text"
+                placeholder="data dodania"
+                onChange={(e) => setDataDodania(e.target.value)}
+                value={dataDodania}
+            />{' '}
+            <br />
+            <input type="text" placeholder="tresc" onChange={(e) => setTresc(e.target.value)} value={tresc} /> <br />
+            <button onClick={addUser}>Dodaj</button>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Autor</th>
+                        <th>Data</th>
+                        <th>Tresc</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user, id) => (
+                        <tr key={id}>
+                            <td>{user.autor}</td>
+                            <td>{user.data_dodania}</td>
+                            <td>{user.tresc}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
-export default App;
+export default App
